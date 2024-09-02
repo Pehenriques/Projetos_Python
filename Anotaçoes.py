f9a900b4ca4e63280a43ac2c8714b020d943ed8b
PEDRO HENRIQUE SILVA FERREIRA <98856651+Pehenriques@users.noreply.github.com>
2024-09-02 20:36:44 -0300

    py

--- Anotaçoes.py
+++ Anotaçoes.py
@@ -270,3 +270,22 @@ curso is limite
 True
 
 """
+
+"""
+
+from datetime import date, datetime, time
+
+data = date(2023, 7, 10)
+print(data)
+print(date.today())
+
+
+data_hora = datetime(2023, 7, 10, 10, 30, 20)
+print(data_hora)
+print(datetime.today())
+
+
+hora = time(10, 20, 0)
+print(hora)
+
+"""
--- Exemplos.py
+++ Exemplos.py
@@ -171,3 +171,19 @@ lanche = ("hamburguer","suco", "pizza","pudim")
     
 #for cont in range(0, len(lanche)):
     #print(lanche[cont])
+    
+#Exemplo de definição de data e hora para diversas opções no Python
+from datetime import date, datetime, time
+
+data = date(2023, 7, 10)
+print(data)
+print(date.today())
+
+
+data_hora = datetime(2023, 7, 10, 10, 30, 20)
+print(data_hora)
+print(datetime.today())
+
+
+hora = time(10, 20, 0)
+print(hora)
